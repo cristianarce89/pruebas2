14acf878b7be97d6ed1e90d4f4cab09200d14f45
Cristian Arce <cristiancamilo_1989@hotmail.com>
2022-09-12 19:20:38 -0500

    version de index 01

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require ('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('slider.js', (req, res) => {
+    res.sendFile(__dirname + '/public/slider.js');
+})
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto  3000 de reeadni")    
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+
