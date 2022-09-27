8b5e857f2b9d32c61286ecb9598ae576ee5594d6
Cristian Arce <cristiancamilo_1989@hotmail.com>
2022-09-26 21:40:05 -0500

    dev

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
+const createError = require('http-errors');
 const express = require('express');
-const path = require ('path');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+var indexRouter = require('./src/routes/index.js');
+// var usersRouter = require('./src/routes/users.js');
+
 const app = express();
 
-app.use(express.static('public'));
+// view engine setup
+app.set('views', path.join(__dirname, '/src/views'));
+app.set('view engine', 'ejs');
 
-app.get('slider.js', (req, res) => {
-    res.sendFile(__dirname + '/public/slider.js');
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en el puerto  3000 de reeadni")    
-})
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/register.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('index');
 });
 
-app.get('/login.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-});
-
-app.get('/productCart.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
-});
-
-app.get('/productDetail.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
-});
-
-app.post("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./views/index.html"))
-})
+module.exports = app;
